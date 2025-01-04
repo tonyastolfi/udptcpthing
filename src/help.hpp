c896ee9a677d0165133e2d416793a38418766302
Anthony Astolfi <tastolfi@gmail.com>
2025-01-04 10:01:20 -0500

    Fix typo.

--- src/help.hpp
+++ src/help.hpp
@@ -6,7 +6,7 @@ inline void show_help()
 {
     std::cerr  //
         << std::setw(16) << "HELP" << std::setw(12) << "(optional)" << " Show this message" << std::endl
-        << std::setw(16) << "MODE" << std::setw(12) << "(required)" << "' udp2tcp' or 'tcp2udp'" << std::endl
+        << std::setw(16) << "MODE" << std::setw(12) << "(required)" << " 'udp2tcp' or 'tcp2udp'" << std::endl
         << std::setw(16) << "UDP_PORT" << std::setw(12) << "(optional)" << " default=19132" << std::endl
         << std::setw(16) << "TCP_PORT" << std::setw(12) << "(optional)" << " default=7777" << std::endl
         << std::setw(16) << "MAX_CLIENTS" << std::setw(12) << "(optional)"
